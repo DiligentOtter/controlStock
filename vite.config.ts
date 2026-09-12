8cc5c347b4cd4d58109b264fe880c99e6dfe73cd
DiligentOtter <jc.sanchez.oliveto@mi.unc.edu.ar>
2026-09-12 18:24:24 -0300

    feat: remove Netlify adapter and clean up configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-netlify';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -7,12 +6,11 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit({
+			// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter()
+			}
 		})
 	]
 });
