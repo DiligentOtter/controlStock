f93618e19e30191a89b63862b04b3809bf7640d1
DiligentOtter <jc.sanchez.oliveto@mi.unc.edu.ar>
2026-09-12 18:30:17 -0300

    feat: update Svelte configuration by moving runes mode to svelte.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-			compilerOptions: {
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
-		})
-	]
+	plugins: [tailwindcss(), sveltekit()]
 });
